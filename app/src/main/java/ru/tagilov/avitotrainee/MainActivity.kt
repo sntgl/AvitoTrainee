8f6134a5b507bcf0f5053f0c766b4580695733c1
Aleksandr Tagilov <saniatagilov@gmail.com>
2021-12-13 23:10:27 +0300

    init

---
+++ app/src/main/java/ru/tagilov/avitotrainee/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.tagilov.avitotrainee
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import coil.annotation.ExperimentalCoilApi
+import ru.tagilov.avitotrainee.ui.component.WeatherIcon
+import ru.tagilov.avitotrainee.ui.theme.AvitoTheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalCoilApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AvitoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Column {
+                        WeatherIcon(icon = "13n")
+                        WeatherIcon(icon = "02d")
+                        WeatherIcon(icon = "02d")
+                        WeatherIcon(icon = "")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AvitoTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/ru/tagilov/avitotrainee/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package ru.tagilov.avitotrainee.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFAA00FF)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Gray100 = Color(0xFFF0F0F0)
+val Gray200 = Color(0xFFD1D1D1)
+val Gray500 = Color(0xFF797979)
+val Gray700 = Color(0xFF414141)

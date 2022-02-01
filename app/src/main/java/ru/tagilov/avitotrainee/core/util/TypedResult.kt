b6aff913e24283de10959d1a0979a8d9758f3837
Aleksandr Tagilov <amtagilov@avito.ru>
2022-02-01 19:13:19 +0300

    fix typed

--- app/src/main/java/ru/tagilov/avitotrainee/core/util/TypedResult.kt
+++ app/src/main/java/ru/tagilov/avitotrainee/core/util/TypedResult.kt
@@ -2,5 +2,5 @@ package ru.tagilov.avitotrainee.core.util
 
 sealed class TypedResult<T> {
     class Err<T> : TypedResult<T>()
-    data class Ok<T>(@Transient val result: T) : TypedResult<T>()
+    data class Ok<T>(val result: T) : TypedResult<T>()
 }

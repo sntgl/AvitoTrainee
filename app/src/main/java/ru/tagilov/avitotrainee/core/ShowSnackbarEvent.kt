71c3d4c16b5638aa750bc2b71ee41f9c989c0c11
Aleksandr Tagilov <amtagilov@avito.ru>
2022-02-11 11:25:34 +0300

    edit ShowSnackbarEvent (equals)

--- app/src/main/java/ru/tagilov/avitotrainee/core/ShowSnackbarEvent.kt
+++ app/src/main/java/ru/tagilov/avitotrainee/core/ShowSnackbarEvent.kt
@@ -1,21 +1,9 @@
 package ru.tagilov.avitotrainee.core
 
-//просто ивент, каждый инстанс которого не равен любым другим инстансам
 sealed class SnackbarEvent {
-    data class Show(
-        val state: SnackBarMessage
-    ) : SnackbarEvent() {
-        override fun equals(other: Any?): Boolean {
-            return other === this
-        }
-
-        override fun hashCode(): Int {
-            return javaClass.hashCode()
-        }
-    }
+    class Show(val state: SnackBarMessage) : SnackbarEvent()
     object Empty : SnackbarEvent()
 }
-
 
 enum class SnackBarMessage {
     UNABLE_LOAD, UNABLE_SAVE
